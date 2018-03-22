b3ec956371e7a7dfe171ab3147a69c3115442a85
ArGJolan <argjolan@gmail.com>
2018-03-22 15:57:40 +0100

    Shit eslint rules & infinite loop is op

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       turn: 'X',
-      gameRunning: props.gameRunning
+      gameRunning: true
     }
   }
 
@@ -41,7 +41,7 @@ class Board extends React.Component {
   renderSquare (i) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.state.squares[i] || ''}
         onClick={() => {
           if (this.state.gameRunning) {
             this.handleClick(i)
@@ -52,7 +52,14 @@ class Board extends React.Component {
   }
 
   render () {
-    const status = `Next player: ${this.state.turn}`;
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `${winner} won the game !`
+      this.setState({ gameRunning: false })
+    } else {
+      status = `Next player: ${this.state.turn}`
+    }
 
     return (
       <div>
@@ -71,23 +78,33 @@ class Board extends React.Component {
   }
 }
 
-Board.propTypes = {
-  gameRunning: PropTypes.bool.isRequired
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      console.log('WE HAVE A WINNER')
+      return squares[a]
+    }
+  }
+  return null
 }
 
 class Game extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      gameRunning: true
-    }
-  }
-
   render () {
     return (
       <div className="game">
         <div className="game-board">
-          <Board gameRunning={this.state.gameRunning} />
+          <Board />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
